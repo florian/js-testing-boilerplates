5d143763538c95ce473b56e90ef46a687732c53a
Florian H <jscoderr@gmail.com>
2013-01-19 15:09:45 +0100

    The same spec for all frameworks

--- mocha-chai/spec/spec.js
+++ mocha-chai/spec/spec.js
@@ -1,5 +1,5 @@
-describe("suite", function () {
-    it("should work", function () {
-        expect(true).to.be.true;
-    });
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).to.be.true;
+  });
 });
